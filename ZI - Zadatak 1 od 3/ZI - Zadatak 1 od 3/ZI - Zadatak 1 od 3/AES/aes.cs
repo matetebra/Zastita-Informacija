40dbefa00b7b6922c31a410ec6d9b39a8a9f1c44
matetebra <95086606+matetebra@users.noreply.github.com>
2021-12-03 00:06:21 +0100

    Add files via upload

---
+++ ZI - Zadatak 1 od 3/ZI - Zadatak 1 od 3/ZI - Zadatak 1 od 3/AES/aes.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Security.Cryptography;
+
+namespace ZI___Zadatak_1_od_3.AES
+{
+    class aes
+    {
+        public static byte[] EncryptStringToBytes_Aes(string plainText, byte[] Key, byte[] IV)
+        {
+            if (plainText == null || plainText.Length <= 0)
+                throw new ArgumentNullException("plainText");
+            if (Key == null || Key.Length <= 0)
+                throw new ArgumentNullException("Key");
+            if (IV == null || IV.Length <= 0)
+                throw new ArgumentNullException("IV");
+            byte[] encrypted;
+   
+            using (AesManaged aes = new AesManaged())
+            { 
+                ICryptoTransform encryptor = aes.CreateEncryptor(Key, IV);
+                
+                using (MemoryStream ms = new MemoryStream())
+                {  
+                    using (CryptoStream cs = new CryptoStream(ms, encryptor, CryptoStreamMode.Write))
+                    {   
+                        using (StreamWriter sw = new StreamWriter(cs))
+                            sw.Write(plainText);
+                        encrypted = ms.ToArray();
+                    }
+                }
+            }  
+
+            return encrypted;
+        }
+
+
+        public static string DecryptStringFromBytes_Aes(byte[] cipherText, byte[] Key, byte[] IV)
+        {
+            if (cipherText == null || cipherText.Length <= 0)
+                throw new ArgumentNullException("cipherText");
+            if (Key == null || Key.Length <= 0)
+                throw new ArgumentNullException("Key");
+            if (IV == null || IV.Length <= 0)
+                throw new ArgumentNullException("IV");
+   
+            string plaintext = null;
+   
+            using (AesManaged aes = new AesManaged())
+            {  
+                ICryptoTransform decryptor = aes.CreateDecryptor(Key, IV);
+                
+                using (MemoryStream ms = new MemoryStream(cipherText))
+                {   
+                    using (CryptoStream cs = new CryptoStream(ms, decryptor, CryptoStreamMode.Read))
+                    { 
+                        using (StreamReader reader = new StreamReader(cs))
+                            plaintext = reader.ReadToEnd();
+                    }
+                }
+            }
+
+            return plaintext;
+        }
+    }
+}
